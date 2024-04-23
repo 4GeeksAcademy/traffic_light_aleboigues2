76d9a5e2237073e05870efe6bba585f62481fa29
Alejandrabalv <48587816+Alejandrabalv@users.noreply.github.com>
2024-04-23 19:01:42 +0000

    traffic light_aleboigues

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -36,7 +36,7 @@ export default function Home() {
         onClick={() => setselectedColor("green")}
         className={"light green" + (selectedColor === "green" ? " glow" : "")
         }></div>
-      <button onClick={changeColor}></button>
+      <button onClick={changeColor}>PRESS</button>
     </div>
   );
 }
